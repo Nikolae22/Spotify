5f1899534895166346699af8b6ecc17973081a6d
Nicolae Moraru <morarunicolae96@gmail.com>
2024-10-22 15:49:23 +0200

    first commit initial commit

---
+++ spotify-back/src/main/java/com/spotify_back/SpotifyBackApplication.java
@@ -0,0 +1,13 @@
+package com.spotify_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyBackApplication.class, args);
+	}
+
+}
---
+++ spotify-back/src/test/java/com/spotify_back/SpotifyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotify_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
